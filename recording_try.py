0a8d3d7f7e3176828cb49d741c838a363c942a82
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-05-25 23:57:49 +0800

    Add files via upload

---
+++ recording_try.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('test.html')
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    audio_file = request.files['audio']
+    if audio_file:
+        audio_file.save("received_audio.wav")  # 保存音訊文件到伺服器
+        return jsonify({'message': '音訊文件上傳成功'})
+    return jsonify({'error': '沒有接收到音訊文件'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
